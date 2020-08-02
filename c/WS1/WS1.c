b49fb1b9212080731d5e884bf0bd29747d321b54
rotem.kadosh <rotemkadosh27@gmail.com>
2020-08-02 11:35:32 +0300

    approved WS1

--- c/WS1/WS1.c
+++ c/WS1/WS1.c
@@ -27,19 +27,21 @@ float exponential(int n)
 	float base;
 	float result;
 	
-	if(n>=38)
+	if (n>=38)
 	{
 		printf("n is to large\n");
 		return -1;
 	}
 	base=10.0;
 	result=1.0;
-	if(n<0)
+
+	if (n<0)
 	{
 		base=0.1;
 		n*=-1;
 	}
-	while(n>0)
+
+	while (n>0)
 	{
 		result*=base;
 		n--;
@@ -54,7 +56,8 @@ int flipInt(int num)
 	
 	reminder=num%10;
 	revers=0;
-	while(num)
+
+	while (num)
 	{
 		reminder=num%10;
 		revers*=10;
@@ -67,19 +70,35 @@ int flipInt(int num)
 void swap (int *num1, int *num2) 
 {
 	int temp;
-	if(NULL==num1 || NULL==num2)
+
+	if (NULL==num1 || NULL==num2)
 	{
 		return;
 	}
+
 	temp=*num1;
 	*num1=*num2;
 	*num2=temp;
 }
 
-		
+	
+/*main function*/
+	
+int main()
+{
+	printHelloHex();
+	testExponential();
+	testFlipInt();
+	testSwap();
+	return 0;
+}
+
+/*tests*/
+
+
 int testExponential()
 {
-	int test1, test2,test3,test4;
+	int test1, test2, test3, test4;
 	
 	test1=(1.0==exponential(0));
 	test2=(1000.0==exponential(3));
@@ -87,11 +106,12 @@ int testExponential()
 	test4=(-1==exponential(50));
 	
 	
-	if(test1 && test2 && test3 && test4)
+	if (test1 && test2 && test3 && test4)
 	{
 		printf("the exponential function passed the tests\n");
 		return 0;
 	}
+
 	else
 	{
 		printf("exponential failed!, test1=%d, test2=%d, test3=%d, test4=%d\n",test1,test2,test3, test4);
@@ -101,7 +121,7 @@ int testExponential()
 
 int testFlipInt()
 {
-	int test1, test2,test3,test4,test5;
+	int test1, test2, test3, test4, test5;
 	
 	test1=(1234==flipInt(4321));
 	test2=(1==flipInt(1000));
@@ -110,11 +130,12 @@ int testFlipInt()
 	test5=((-123)==flipInt(-321));
 	
 	
-	if(test1 && test2 && test3 && test4 && test5)
+	if (test1 && test2 && test3 && test4 && test5)
 	{
 		printf("the flipInt function passed the tests\n");
 		return 0;
 	}
+
 	else
 	{
 		printf("flipInt failed!, test1=%d, test2=%d, test3=%d, test4=%d, test5=%d\n",test1,test2,test3,test4,test5);
@@ -126,19 +147,14 @@ int testSwap()
 {
 	int a, b, c,d,e;
 	int test1, test2, test3;
-	
-	a=4;
-	b=3;
-	c=2;
-	d=1;
-	e=5;
+	a=4, b=3, c=2, d=1, e=5;
 	swap(&a,&d);
 	swap(&b,&c);
 	swap(&e,NULL);
 	test1=(2==b && 3==c);
 	test2=(1==a && 4==d);
 	test3=(5==e);
-	if(test1 && test2 && test3)
+	if (test1 && test2 && test3)
 	{
 		printf("the swap function passed the tests\n");
 		return 0;
@@ -148,17 +164,6 @@ int testSwap()
 		printf("swap failed!, test1=%d, test2=%d, test3=%d\n",test1,test2,test3);
 		return -1;
 	}
-}
-	
-/*main function*/
-	
-int main()
-{
-	printHelloHex();
-	testExponential();
-	testFlipInt();
-	testSwap();
-	return 0;
 }
 
 
