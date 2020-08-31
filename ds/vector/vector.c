32f54b5e33480cf16fca0813c46226af063a0daa
rotem.kadosh <rotemkadosh27@gmail.com>
2020-08-31 15:33:35 +0300

    implemention tested

---
+++ ds/vector/vector.c
@@ -0,0 +1,108 @@
+#include <stdlib.h> /*malloc, free*/
+#include <assert.h> /*assert*/
+#include "vector.h"
+
+#define GROWTH_FACTOR (2)
+
+struct vector
+{
+    void **items;
+    size_t capacity;
+    size_t last;
+}; 
+
+Vector_t *VectorCreate(size_t capacity)
+{
+	Vector_t *vec = NULL;
+	assert(0 < capacity);
+	vec = (Vector_t *)malloc(sizeof(Vector_t));
+	if(NULL == vec)
+	{
+		return NULL;
+	}
+	vec->items = (void **)malloc(capacity * sizeof(void *));
+	if(NULL == vec->items)
+	{
+		return NULL;
+	}
+	vec->capacity = capacity;
+	vec->last = 0;
+	return vec;
+}
+void VectorDestroy(Vector_t *vector)
+{
+	assert(NULL != vector);
+	vector->items = NULL;
+	free(vector);
+	vector = NULL;
+}
+void *VectorGetElement(const Vector_t *vector, size_t index)
+{
+	assert(NULL != vector);
+	return vector->items[index];
+}
+void VectorSetElement(Vector_t *vector, void *element, size_t index)
+{
+	assert(NULL != vector);
+	vector->items[index] = element;
+}
+size_t VectorSize(const Vector_t *vector)
+{
+	assert(NULL != vector);
+	return vector->last ;
+}
+size_t VectorCapacity(const Vector_t *vector)
+{
+	assert(NULL != vector);
+	return vector->capacity;
+}
+int VectorPushBack(Vector_t *vector, void *element)
+{
+	size_t new_capacity = 0;
+	assert(NULL != vector);
+	if(vector->last >= vector->capacity)
+	{
+		new_capacity = vector->capacity * GROWTH_FACTOR;
+
+		if(0 != VectorReserve(vector , new_capacity))
+		{
+			return -1;
+		}
+		vector->capacity = new_capacity;
+	}
+	vector->items[vector->last++] = element;
+	return 0;
+
+}
+void VectorPopBack(Vector_t *vector)
+{
+	assert(NULL != vector);
+	--vector->last;
+}
+int VectorReserve(Vector_t *vector, size_t new_capacity)
+{
+	assert(NULL != vector);
+	vector->items = (void **)realloc( vector->items , new_capacity * sizeof(void *));
+	if(NULL == vector->items)
+	{
+		return -1;
+	}
+	vector->capacity = new_capacity;
+	return 0;
+}
+int VectorShrinkToFit(Vector_t *vector)
+{
+	assert(NULL != vector);
+	if(0 != VectorReserve(vector, vector->last))
+	{
+		return -1;
+	}
+	return 0;
+	
+}
+
+
+
+
+
+
---
+++ ds/vector/vector_test.c
@@ -0,0 +1,132 @@
+#include "test.h" /*REQUIER, RUNTEST*/
+#include "vector.h"
+
+
+static test_status_t TestVectorCreate();
+static test_status_t TestVectorPushBack();
+static test_status_t TestVectorPopBack();
+static test_status_t TestVectorGetElement();
+static test_status_t TestVectorSetElement();
+static test_status_t TestVectorReserve();
+static test_status_t TestVectorShrinkToFit();
+
+int main(int argc, char const *argv[])
+{
+	
+	RUNTEST(TestVectorCreate);
+	RUNTEST(TestVectorPushBack);
+	RUNTEST(TestVectorPopBack);
+	RUNTEST(TestVectorGetElement);
+	RUNTEST(TestVectorSetElement);
+	RUNTEST(TestVectorReserve);
+	RUNTEST(TestVectorShrinkToFit);
+
+	(void) argc;
+	(void) argv;
+
+	return 0;
+}
+
+
+static test_status_t TestVectorCreate()
+{
+	Vector_t *vec = VectorCreate(20);
+	REQUIRE(NULL != vec);
+	VectorDestroy(vec);
+	vec = VectorCreate(1);
+	REQUIRE(NULL != vec);
+	VectorDestroy(vec);
+	vec = VectorCreate(15000);
+	REQUIRE(NULL != vec);
+	VectorDestroy(vec);
+	return PASSED;
+}
+static test_status_t TestVectorPushBack()
+{
+	Vector_t *vec = VectorCreate(1);
+	REQUIRE(0 ==  VectorPushBack(vec, (void *)8));
+	REQUIRE(0 ==  VectorPushBack(vec, (void *)7));
+	REQUIRE(0 ==  VectorPushBack(vec, (void *)6));
+	REQUIRE(0 ==  VectorPushBack(vec, (void *)'H'));
+	VectorDestroy(vec);
+	return PASSED;
+}
+static test_status_t TestVectorPopBack()
+{
+	Vector_t *vec = VectorCreate(1);
+	VectorPushBack(vec, (void *)8);
+	VectorPushBack(vec, (void *)7);
+	VectorPushBack(vec, (void *)6);
+	VectorPushBack(vec, (void *)'H');
+	
+	REQUIRE(4 ==  VectorSize(vec));
+	VectorPopBack(vec);
+	REQUIRE(3 ==  VectorSize(vec));
+	VectorPopBack(vec);
+	REQUIRE(2 ==  VectorSize(vec));
+	VectorPopBack(vec);
+	REQUIRE(1 ==  VectorSize(vec));
+	VectorPopBack(vec);
+	REQUIRE(0 ==  VectorSize(vec));
+	VectorDestroy(vec);
+	return PASSED;
+}
+static test_status_t TestVectorSetElement()
+{
+	Vector_t *vec = VectorCreate(1);
+	VectorPushBack(vec, (void *)8);
+	VectorPushBack(vec, (void *)7);
+	VectorPushBack(vec, (void *)6);
+	
+	VectorSetElement(vec,(void *)7, 0);
+	VectorSetElement(vec,(void *)7, 1);
+	VectorSetElement(vec,(void *)7, 2);
+	VectorSetElement(vec,(void *)7, 3);
+	
+	REQUIRE((void *)7 ==  VectorGetElement(vec, 0));
+	REQUIRE((void *)7 ==  VectorGetElement(vec, 1));
+	REQUIRE((void *)7 ==  VectorGetElement(vec, 2));
+	REQUIRE((void *)7 ==  VectorGetElement(vec, 3));
+
+	VectorDestroy(vec);
+	return PASSED;
+}
+static test_status_t TestVectorGetElement()
+{
+	Vector_t *vec = VectorCreate(1);
+	VectorPushBack(vec, (void *)8);
+	VectorPushBack(vec, (void *)7);
+	VectorPushBack(vec, (void *)6);
+	VectorPushBack(vec, (void *)'H');
+	
+	REQUIRE((void *)8 ==  VectorGetElement(vec, 0));
+	REQUIRE((void *)7 ==  VectorGetElement(vec, 1));
+	REQUIRE((void *)6 ==  VectorGetElement(vec, 2));
+	REQUIRE((void *)'H' ==  VectorGetElement(vec, 3));
+
+	VectorDestroy(vec);
+	return PASSED;
+}
+static test_status_t TestVectorReserve()
+{
+	Vector_t *vec = VectorCreate(20);
+	REQUIRE(0 == VectorReserve(vec, 24));
+	REQUIRE(0 == VectorReserve(vec, 26));
+	REQUIRE(0 == VectorReserve(vec, 28));
+	REQUIRE(0 == VectorReserve(vec, 20));
+	VectorDestroy(vec);
+	return PASSED;
+}
+
+static test_status_t TestVectorShrinkToFit()
+{
+	Vector_t *vec = VectorCreate(20);
+	VectorPushBack(vec, (void *)8);
+	VectorPushBack(vec, (void *)7);
+	VectorPushBack(vec, (void *)6);
+	VectorPushBack(vec, (void *)'H');
+	REQUIRE(0 == VectorShrinkToFit(vec));
+	REQUIRE(4 == VectorCapacity(vec));
+	VectorDestroy(vec);
+	return PASSED;
+}
