29e37ee3f53582b92760e8f83d9d70cfb2478a0f
rotem.kadosh <rotemkadosh27@gmail.com>
2020-09-02 15:58:06 +0300

    approved by may

--- ds/slist/slist.c
+++ ds/slist/slist.c
@@ -1,8 +1,10 @@
-#include <stdlib.h>/*malloc, free*/
+#include <stdlib.h> /*malloc, free*/
+#include <assert.h> /*assert*/
 #include "slist.h"
 #define TRUE (1)
 #define FALSE (0)
 
+/*approved by may*/
 
 typedef struct slist_node
 {
@@ -18,38 +20,39 @@ struct slist
 
 
 static int CountFunc(void *data, void *param);
-static Slist_iter_t CreateNode(void *value);
+static slist_node_t *CreateNode(void *value);
 static Slist_iter_t TraversToEnd(Slist_iter_t iter);
 
 
 static Slist_iter_t CreateNode(void *value)
-{
+{	
 	slist_node_t *new_node = (slist_node_t *)malloc(sizeof(slist_node_t));
 	if(NULL == new_node)
 	{
 		return NULL;
 	}
 	new_node->value = value;
-	return (Slist_iter_t)new_node;
+	return new_node;
 }
 Slist_iter_t SlistBegin(const Slist_t *list)
 {
-
+	assert(NULL != list);
 	return list->head;
 }
 Slist_iter_t SlistEnd(const Slist_t *list)
 {
-
-	return (Slist_iter_t)list->tail;
+	assert(NULL != list);
+	return list->tail;
 }
 Slist_iter_t SlistNext(Slist_iter_t iter)
-{
-	slist_node_t *iter_node = (slist_node_t *)iter;
-	return iter_node->next;
+{	
+	assert(NULL != iter);
+	return iter->next;
 }
 int SlistIsSameIter(Slist_iter_t one, Slist_iter_t two)
 {
-
+	assert(NULL != one);
+	assert(NULL != two);
 	if(one->value == two->value && one->next == two->next)
 	{
 		return TRUE;
@@ -59,7 +62,7 @@ int SlistIsSameIter(Slist_iter_t one, Slist_iter_t two)
 Slist_t *SlistCreate(void)
 {
 	Slist_t *list = NULL;
-	Slist_iter_t dummy = NULL;
+	slist_node_t *dummy = NULL;
 	list = (Slist_t *)malloc(sizeof(Slist_t));
 	if (NULL == list)
 	{
@@ -82,29 +85,34 @@ Slist_t *SlistCreate(void)
 
 void SlistDestroy(Slist_t *list)
 {
-	Slist_iter_t iter_current = list->head;
-	Slist_iter_t iter_next = iter_current->next;
+	Slist_iter_t iter_current = NULL;
+	Slist_iter_t iter_next = NULL;
+
+	assert(NULL != list);
+
+	iter_current = list->head;
 	do
 	{
 		iter_next = iter_current->next;
 		free(iter_current);
 		iter_current = iter_next;
-	}while (NULL != iter_next);
+	} while (NULL != iter_next);
 	free(list);
 }
 void *SlistGetData(const Slist_iter_t iter)
 {
-
+	assert(NULL != iter);
 	return iter->value;
 }
 void SlistSetData(Slist_iter_t iter, void *data)
 {
-
+	assert(NULL != iter);
 	iter->value = data;
 }
 static Slist_iter_t TraversToEnd(Slist_iter_t iter)
 {
-	while(NULL != iter->next)
+
+	while (NULL != iter->next)
 	{
 		iter = iter->next;
 	}
@@ -112,8 +120,12 @@ static Slist_iter_t TraversToEnd(Slist_iter_t iter)
 }
 Slist_iter_t SlistInsert(Slist_iter_t where, void *data)
 {
-	Slist_iter_t new_node = CreateNode(where->value);
-	if(NULL == new_node)
+	Slist_iter_t new_node = NULL;
+	assert(NULL != where);
+
+	new_node = CreateNode(where->value);
+
+	if (NULL == new_node)
 	{
 		return TraversToEnd(where);
 	}
@@ -122,25 +134,33 @@ Slist_iter_t SlistInsert(Slist_iter_t where, void *data)
         ((Slist_t *)where->value)->tail = new_node;
     
 	}
+
 	new_node->next = where->next;
-	SlistSetData(where, data);
+
+	where->value = data;
 	where->next = new_node;
+
 	return where;
 }
 Slist_iter_t SlistRemove(Slist_iter_t iter)
 {
 	Slist_iter_t next = NULL;
-	if(NULL == iter->next)
+	assert(NULL != iter);
+	if (NULL == iter->next)
 	{
 		return iter;
 	}
+
 	next = iter->next;
 	iter->value = next->value;
 	iter->next = next->next;
 	free(next);
-	if(NULL == iter->next){
-		 ((Slist_t *)iter->value)->tail = iter;
+
+	if (NULL == iter->next)
+	{
+ 		((Slist_t *)iter->value)->tail = iter;
 	}
+
 	return iter;
 }
 static int CountFunc(void *data, void *param)
@@ -151,18 +171,19 @@ static int CountFunc(void *data, void *param)
 }
 size_t SlistCount(const Slist_t *list)
 {
+
 	size_t count = 0;
-	Slist_iter_t iter_begin = NULL;
-	Slist_iter_t iter_End = NULL;
-	iter_begin = SlistBegin(list);
-	iter_End = SlistEnd(list);
-	SlistForEach(iter_begin, iter_End, CountFunc, &count);
+
+	assert(NULL != list);
+	 
+	SlistForEach(SlistBegin(list), SlistEnd(list), CountFunc, &count);
+
 	return count;
 }
 
 Slist_iter_t SlistFind(Slist_iter_t from, Slist_iter_t to,const void *data_to_compare, is_match_func_t is_match_func)
 {
-	while(!SlistIsSameIter(from, to) && !is_match_func(from->value, data_to_compare))
+	while (!SlistIsSameIter(from, to) && !is_match_func(from->value, data_to_compare))
 	{
 		from = from->next;
 	}
@@ -171,7 +192,7 @@ Slist_iter_t SlistFind(Slist_iter_t from, Slist_iter_t to,const void *data_to_co
 int SlistForEach(Slist_iter_t from, Slist_iter_t to, action_func_t action_func, void *param)
 {
 	int res = 0;
-	while(!SlistIsSameIter(from, to))
+	while (!SlistIsSameIter(from, to))
 	{
 		res = action_func(from->value, param);
 		from = from->next;
