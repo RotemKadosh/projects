f800d0e3c55ad38580b23e5359a71f2e5816c2ec
rotem.kadosh <rotemkadosh27@gmail.com>
2020-09-13 12:14:09 +0300

    dlist tested approved by moshe

---
+++ ds/dlist/dlist.c
@@ -0,0 +1,249 @@
+#include <stdlib.h>/*malloc, free*/
+#include <assert.h>/*assert*/
+#include "dlist.h"
+
+#define SUCCESS (0)
+#define FAIL (-1)
+
+typedef struct dlist_node dlist_node_t;
+
+static dlist_node_t *CreateNode(void *value);
+static void ConectNodes(Dlist_iter_t first, Dlist_iter_t second);
+
+struct dlist_node
+{
+	void *value;
+	dlist_node_t *next;
+	dlist_node_t *prev;
+
+};
+
+struct dlist
+{
+	dlist_node_t head_dummy;
+	dlist_node_t tail_dummy;
+};
+
+static dlist_node_t *CreateNode(void *value)
+{	
+	dlist_node_t *new_node = (dlist_node_t *)malloc(sizeof(dlist_node_t));
+	if(NULL == new_node)
+	{
+		return NULL;
+	}
+	new_node->value = value;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	return new_node;
+}
+Dlist_t *DlistCreate(void)
+{
+	Dlist_t *list = NULL;
+	list = (Dlist_t *)malloc(sizeof(Dlist_t));
+	if (NULL == list)
+	{
+		return NULL;
+	}
+	DlistSetData(&list->tail_dummy, (void *)list);
+	DlistSetData(&list->head_dummy, (void *)list);
+
+	list->head_dummy.prev = NULL;
+	ConectNodes(&list->head_dummy, &list->tail_dummy);
+	list->tail_dummy.next = NULL;
+
+	return list;
+}
+void DlistDestroy(Dlist_t *list)
+{
+	Dlist_iter_t iter_current = NULL;
+	Dlist_iter_t iter_next = NULL;
+
+	assert(NULL != list);
+
+	iter_current =DlistNext(&list->head_dummy);
+	while (NULL != iter_current->next)
+	{
+		iter_next =DlistNext(iter_current);
+		free(iter_current);
+		iter_current = NULL;
+		iter_current = iter_next;
+	} 
+
+	free(list);
+	list = NULL;
+}
+Dlist_iter_t DlistBegin(const Dlist_t *list)
+{
+	assert(NULL != list);
+	return list->head_dummy.next;
+}
+Dlist_iter_t DlistEnd(const Dlist_t *list)
+{
+	assert(NULL != list);
+	return list->tail_dummy.prev->next;
+}
+int DlistIsSameIter(Dlist_iter_t one, Dlist_iter_t two)
+{
+
+	return (one == two);
+}
+void *DlistGetData(const Dlist_iter_t iter)
+{
+	assert(NULL != iter);
+	return iter->value;
+}
+Dlist_iter_t DlistInsert(Dlist_t *list, Dlist_iter_t where, void *data)
+{
+	Dlist_iter_t new_node = NULL;
+	assert(NULL != where);
+	assert(NULL != list);
+	new_node = CreateNode(data);
+	if (NULL == new_node)
+	{
+		return DlistEnd(list);
+	}
+	ConectNodes(DlistPrev(where), new_node);
+	ConectNodes(new_node, where);
+	return new_node;
+}
+int DlistIsEmpty(const Dlist_t *list)
+{
+	assert(NULL != list);
+	return DlistIsSameIter(DlistBegin(list), DlistEnd(list));
+}
+Dlist_iter_t DlistNext(Dlist_iter_t iter)
+{
+	assert(NULL != iter);
+	assert(NULL != iter->next);
+	return iter->next;
+}
+Dlist_iter_t DlistPrev(Dlist_iter_t iter)
+{
+	assert(NULL != iter);
+	assert(NULL != iter->prev);
+	return iter->prev;
+}
+void DlistSetData(Dlist_iter_t iter, void *data)
+{
+	assert(NULL != iter);
+	iter->value = data;
+}
+Dlist_iter_t DlistRemove(Dlist_t *list, Dlist_iter_t iter)
+{
+	dlist_node_t *next = NULL;
+	assert(NULL != iter);
+	assert(NULL != iter->next);
+
+	ConectNodes(DlistPrev(iter), DlistNext(iter));
+	next = iter->next;
+	free(iter);
+	iter = NULL;
+	(void) list;
+	return (Dlist_iter_t)next;
+}
+
+Dlist_iter_t DlistPushFront(Dlist_t *list, void *data)
+{
+	assert(NULL != list);
+	return DlistInsert(list, DlistBegin(list), data);
+}
+
+Dlist_iter_t DlistPushBack(Dlist_t *list, void *data)
+{
+	assert(NULL != list);
+	return DlistInsert(list, DlistEnd(list), data);
+}
+
+void DlistPopFront(Dlist_t *list)
+{
+	assert(NULL != list);
+	DlistRemove(list, DlistBegin(list));
+}
+
+void DlistPopBack(Dlist_t *list)
+{
+	assert(NULL != list);
+	DlistRemove(list, DlistPrev(DlistEnd(list)));
+}
+size_t DlistSize(const Dlist_t *list)
+{
+	Dlist_iter_t runner = NULL;
+	size_t count = 0;
+	assert(NULL != list);
+	runner = DlistBegin(list);
+	while(!DlistIsSameIter(runner, DlistEnd(list)))
+	{
+		++count;
+		runner = runner->next;
+	}
+	return count;
+}
+
+int DlistForEach(Dlist_iter_t from, Dlist_iter_t to, action_func_t action_func, void *param)
+{
+	Dlist_iter_t runner = from;
+	int action_status = SUCCESS;
+	assert(NULL != from);
+	assert(NULL != action_func);
+
+	while(!DlistIsSameIter(runner, to) && SUCCESS == action_status)
+	{
+		action_status = action_func(runner->value, param);
+		runner = runner->next;
+	}
+	return action_status;
+}
+Dlist_iter_t DlistFind(Dlist_iter_t from, Dlist_iter_t to, is_match_func_t is_match_func, const void *data_to_compare)
+{
+	Dlist_iter_t runner = from;
+	assert(NULL != from);
+
+	while (!DlistIsSameIter(runner, to) && !is_match_func(DlistGetData(runner), data_to_compare))
+	{
+		runner = DlistNext(runner);
+	}
+	return runner;
+}
+int DlistMultiFind(Dlist_t *dest, Dlist_iter_t from, Dlist_iter_t to, is_match_func_t is_match_func, const void *data_to_compare)
+{
+	Dlist_iter_t runner = from;
+	assert(NULL != from);
+	assert(NULL != to);
+	assert(NULL != dest);
+	while (!DlistIsSameIter(runner, to))
+	{
+		if(is_match_func(DlistGetData(runner), data_to_compare))
+		{
+			if(NULL == DlistPushBack(dest, DlistGetData(runner)))
+			{
+				return FAIL;
+			}
+		}
+		runner = DlistNext(runner);
+	}
+	return SUCCESS;
+}
+
+Dlist_iter_t DlistSplice(Dlist_iter_t where, Dlist_iter_t src_from, Dlist_iter_t src_to)
+{
+	dlist_node_t *form_prev = NULL;
+	assert(NULL != src_from);
+	assert(NULL != src_to);
+	assert(NULL != where);
+
+	form_prev = DlistPrev(src_from);
+	/*attaching to new list*/
+	ConectNodes(DlistPrev(where), src_from);
+	ConectNodes(DlistPrev(src_to), where);
+	/*cut out from old list*/
+	ConectNodes(form_prev, src_to);
+
+	
+
+	return	src_to;
+}
+static void ConectNodes(dlist_node_t *first, dlist_node_t *second)
+{
+	first->next = second;
+	second->prev = first;
+}
---
+++ ds/dlist/dlist_test.c
@@ -0,0 +1,426 @@
+#include "../utils/test.h"
+#include "dlist.h"
+
+#define TRUE (1)
+#define FALSE (0)
+
+static test_status_t DlistCreateTest();
+static test_status_t DlistIsEmptyTest();
+static test_status_t DlistSizeTest();
+static test_status_t DlistInsertTest();
+static test_status_t DlistRemoveTest();
+static test_status_t DlistPushFrontTest();
+static test_status_t DlistPushBackTest();
+static test_status_t DlistPopFrontTest();
+static test_status_t DlistPopBackTest();
+static test_status_t DlistBeginTest();
+static test_status_t DlistEndTest();
+static test_status_t DlistIsSameIterTest();
+static test_status_t DlistNextTest();
+static test_status_t DlistPrevTest();
+static test_status_t DlistGetDataTest();
+static test_status_t DlistSetDataTest();
+static test_status_t DlistFindTest();
+static test_status_t DlistMultiFindTest();
+static test_status_t DlistForEachTest();
+static test_status_t DlistSpliceTest();
+
+int UpDataByPram(void *data, void *param);
+int MatchInt(const void *data, const void *data_to_compare);
+
+int main()
+{
+	RUNTEST(DlistCreateTest);
+	RUNTEST(DlistIsEmptyTest);
+	RUNTEST(DlistSizeTest);
+	RUNTEST(DlistInsertTest);
+	RUNTEST(DlistRemoveTest);
+	RUNTEST(DlistPushFrontTest);
+	RUNTEST(DlistPushBackTest);
+	RUNTEST(DlistPopFrontTest);
+	RUNTEST(DlistPopBackTest);
+	RUNTEST(DlistBeginTest);
+	RUNTEST(DlistEndTest);
+	RUNTEST(DlistIsSameIterTest);
+	RUNTEST(DlistNextTest);
+	RUNTEST(DlistPrevTest);
+	RUNTEST(DlistGetDataTest);
+	RUNTEST(DlistSetDataTest);
+	RUNTEST(DlistFindTest);
+	RUNTEST(DlistMultiFindTest);
+	RUNTEST(DlistForEachTest);
+	RUNTEST(DlistSpliceTest);
+
+
+return 0; 
+}
+
+int UpDataByPram(void *data, void *param)
+{
+	*(int *)data += *(int *)param;
+	return 0;
+}
+int MatchInt(const void *data, const void *data_to_compare)
+{
+
+	return *(int *)data == *(int *)data_to_compare;
+}
+
+static test_status_t DlistCreateTest()
+{
+	Dlist_t *list = DlistCreate();
+	REQUIRE(NULL != list);
+	DlistDestroy(list);
+	return PASSED;
+}
+static test_status_t DlistIsEmptyTest()
+{
+	Dlist_t *list = DlistCreate();
+	REQUIRE(TRUE == DlistIsEmpty(list));
+	DlistInsert(list, DlistBegin(list), (void *)14);
+	REQUIRE(FALSE == DlistIsEmpty(list));
+	DlistDestroy(list);
+	return PASSED;
+}
+static test_status_t DlistInsertTest()
+{
+	Dlist_t *list = DlistCreate();
+	Dlist_iter_t where = DlistBegin(list);
+	Dlist_iter_t inserted = DlistInsert(list, where,(void *)8);
+	REQUIRE(8 == (int)(size_t)DlistGetData(inserted));
+	inserted = DlistInsert(list, where,(void *)8);
+	REQUIRE(8 == (int)(size_t)DlistGetData(inserted));
+	inserted = DlistInsert(list, where,(void *)'c');
+	REQUIRE('c' == (int)(size_t)DlistGetData(inserted));
+	inserted = DlistInsert(list, where,(void *)'g');
+	REQUIRE('g' == (int)(size_t)DlistGetData(inserted));
+	inserted = DlistInsert(list, where,(void *)100);
+	REQUIRE(100 == (int)(size_t)DlistGetData(inserted));
+	DlistDestroy(list);
+	return PASSED;
+}
+static test_status_t DlistBeginTest()
+{
+	Dlist_t *list = DlistCreate();
+	DlistInsert(list, DlistBegin(list), (void *)8);
+	REQUIRE((void *)8 == DlistGetData(DlistBegin(list)));
+	DlistInsert(list, DlistBegin(list), (void *)9);
+	REQUIRE((void *)9 == DlistGetData(DlistBegin(list)));
+	DlistInsert(list, DlistBegin(list), (void *)'c');
+	REQUIRE((void *)'c' == DlistGetData(DlistBegin(list)));
+	DlistInsert(list, DlistBegin(list), (void *)'8');
+	REQUIRE((void *)'8' == DlistGetData(DlistBegin(list)));
+	DlistDestroy(list);
+	return PASSED;
+}
+static test_status_t DlistEndTest()
+{
+	Dlist_t *list = DlistCreate();
+	DlistInsert(list, DlistEnd(list), (void *)8);
+	REQUIRE((void *)list == DlistGetData(DlistEnd(list)));
+	DlistInsert(list, DlistEnd(list), (void *)9);
+	REQUIRE((void *)list == DlistGetData(DlistEnd(list)));
+	DlistInsert(list, DlistEnd(list), (void *)'c');
+	REQUIRE((void *)list == DlistGetData(DlistEnd(list)));
+	DlistInsert(list, DlistEnd(list), (void *)'8');
+	REQUIRE((void *)list == DlistGetData(DlistEnd(list)));
+	DlistDestroy(list);
+	return PASSED;
+}
+static test_status_t DlistNextTest()
+{
+	Dlist_t *list = DlistCreate();
+	Dlist_iter_t inserted = NULL;
+	inserted = DlistInsert(list, DlistEnd(list), (void *)9);
+	inserted = DlistInsert(list, DlistEnd(list), (void *)9);
+	REQUIRE(inserted == DlistNext(DlistBegin(list)));
+	inserted = DlistInsert(list, DlistBegin(list), (void *)9);
+	REQUIRE(DlistNext(inserted) == DlistNext(DlistBegin(list)));
+	DlistDestroy(list);
+	return PASSED;
+}
+static test_status_t DlistPrevTest()
+{
+	Dlist_t *list = DlistCreate();
+	Dlist_iter_t inserted = NULL;
+	Dlist_iter_t prev = NULL;
+	inserted = DlistInsert(list, DlistEnd(list), (void *)1);
+	inserted = DlistInsert(list, DlistEnd(list), (void *)2);
+	inserted = DlistInsert(list, DlistEnd(list), (void *)3);
+	inserted = DlistInsert(list, DlistEnd(list), (void *)4);
+	prev = DlistPrev(inserted);
+	REQUIRE((void *)3 == DlistGetData(prev));
+	prev = DlistPrev(prev);
+	REQUIRE((void *)2 == DlistGetData(prev));
+	prev = DlistPrev(prev);
+	REQUIRE((void *)1 == DlistGetData(prev));
+	DlistDestroy(list);
+	return PASSED;
+}
+static test_status_t DlistIsSameIterTest()
+{
+	Dlist_t *list = DlistCreate();
+	REQUIRE(TRUE == DlistIsSameIter(DlistBegin(list),DlistEnd(list)));
+	DlistInsert(list, DlistBegin(list), (void *)'v');
+	REQUIRE(FALSE == DlistIsSameIter(DlistBegin(list),DlistEnd(list)));
+	DlistDestroy(list);
+	return PASSED;
+}
+static test_status_t DlistGetDataTest()
+{
+	Dlist_t *list = DlistCreate();
+	Dlist_iter_t where = DlistBegin(list);
+	Dlist_iter_t inserted = DlistInsert(list, where,(void *)8);
+	REQUIRE(8 == (int)(size_t)DlistGetData(inserted));
+	inserted = DlistInsert(list, where,(void *)8);
+	REQUIRE(8 == (int)(size_t)DlistGetData(inserted));
+	inserted = DlistInsert(list, where,(void *)'c');
+	REQUIRE('c' == (int)(size_t)DlistGetData(inserted));
+	inserted = DlistInsert(list, where,(void *)'g');
+	REQUIRE('g' == (int)(size_t)DlistGetData(inserted));
+	inserted = DlistInsert(list, where,(void *)100);
+	REQUIRE(100 == (int)(size_t)DlistGetData(inserted));
+	DlistDestroy(list);
+	return PASSED;
+}
+static test_status_t DlistSetDataTest()
+{
+	Dlist_t *list = DlistCreate();
+	Dlist_iter_t where = DlistBegin(list);
+	Dlist_iter_t inserted = DlistInsert(list, where,(void *)8);
+	DlistSetData(inserted, (void *)'c');
+	REQUIRE((void *)'c' == DlistGetData(inserted));
+	DlistSetData(inserted, (void *)'d');
+	REQUIRE((void *)'d' == DlistGetData(inserted));
+	DlistSetData(inserted, (void *)25);
+	REQUIRE((void *)25 == DlistGetData(inserted));
+	DlistSetData(inserted, (void *)1);
+	REQUIRE((void *)1 == DlistGetData(inserted));
+	DlistSetData(inserted, (void *)'c');
+	REQUIRE((void *)'c' == DlistGetData(inserted));
+	DlistDestroy(list);
+	return PASSED;
+}
+static test_status_t DlistRemoveTest()
+{
+	Dlist_t *list = DlistCreate();
+	Dlist_iter_t where = DlistInsert(list, DlistBegin(list), (void *)14);
+	REQUIRE(FALSE == DlistIsEmpty(list));
+	DlistRemove(list, where);
+	REQUIRE(TRUE == DlistIsEmpty(list));
+	where = DlistInsert(list, DlistBegin(list), (void *)14);
+	REQUIRE(FALSE == DlistIsEmpty(list));
+	DlistRemove(list, where);
+	REQUIRE(TRUE == DlistIsEmpty(list));
+	DlistDestroy(list);
+	return PASSED;	
+}
+static test_status_t DlistPushFrontTest()
+{
+	Dlist_t *list = DlistCreate();
+	DlistPushFront(list, (void *)8);
+	REQUIRE((void *)8 == DlistGetData(DlistBegin(list)));
+	DlistPushFront(list, (void *)9);
+	REQUIRE((void *)9 == DlistGetData(DlistBegin(list)));
+	DlistPushFront(list, (void *)10);
+	REQUIRE((void *)10 == DlistGetData(DlistBegin(list)));
+	DlistPushFront(list, (void *)6);
+	REQUIRE((void *)6 == DlistGetData(DlistBegin(list)));
+	DlistPushFront(list, (void *)'c');
+	REQUIRE((void *)'c' == DlistGetData(DlistBegin(list)));
+	DlistPushFront(list, (void *)'h');
+	REQUIRE((void *)'h' == DlistGetData(DlistBegin(list)));
+	DlistDestroy(list);
+	return PASSED;
+}
+static test_status_t DlistPushBackTest()
+{
+	Dlist_t *list = DlistCreate();
+	DlistPushBack(list, (void *)8);
+	REQUIRE((void *)8 == DlistGetData(DlistPrev(DlistEnd(list))));
+	DlistPushBack(list, (void *)9);
+	REQUIRE((void *)9 == DlistGetData(DlistPrev(DlistEnd(list))));
+	DlistPushBack(list, (void *)10);
+	REQUIRE((void *)10 == DlistGetData(DlistPrev(DlistEnd(list))));
+	DlistPushBack(list, (void *)6);
+	REQUIRE((void *)6 == DlistGetData(DlistPrev(DlistEnd(list))));
+	DlistPushBack(list, (void *)'c');
+	REQUIRE((void *)'c' == DlistGetData(DlistPrev(DlistEnd(list))));
+	DlistPushBack(list, (void *)'h');
+	REQUIRE((void *)'h' == DlistGetData(DlistPrev(DlistEnd(list))));
+	DlistDestroy(list);
+	return PASSED;
+}
+static test_status_t DlistPopFrontTest()
+{
+	Dlist_t *list = DlistCreate();
+	DlistPushFront(list, (void *)8);
+	DlistPushFront(list, (void *)9);
+	DlistPushFront(list, (void *)10);
+	DlistPushFront(list, (void *)6);
+	DlistPushFront(list, (void *)'c');
+	DlistPushFront(list, (void *)'h');
+	REQUIRE((void *)'h' == DlistGetData(DlistBegin(list)));
+	DlistPopFront(list);
+	REQUIRE((void *)'c' == DlistGetData(DlistBegin(list)));
+	DlistPopFront(list);
+	REQUIRE((void *)6 == DlistGetData(DlistBegin(list)));
+	DlistPopFront(list);
+	REQUIRE((void *)10 == DlistGetData(DlistBegin(list)));
+	DlistPopFront(list);
+	REQUIRE((void *)9 == DlistGetData(DlistBegin(list)));
+	DlistPopFront(list);
+	REQUIRE((void *)8 == DlistGetData(DlistBegin(list)));
+	DlistDestroy(list);
+	return PASSED;
+}
+static test_status_t DlistPopBackTest()
+{
+	Dlist_t *list = DlistCreate();
+	DlistPushBack(list, (void *)8);
+	DlistPushBack(list, (void *)9);
+	DlistPushBack(list, (void *)10);
+	DlistPushBack(list, (void *)6);
+	DlistPushBack(list, (void *)'c');
+	DlistPushBack(list, (void *)'h');
+	REQUIRE((void *)'h' == DlistGetData(DlistPrev(DlistEnd(list))));
+	DlistPopBack(list);
+	REQUIRE((void *)'c' == DlistGetData(DlistPrev(DlistEnd(list))));
+	DlistPopBack(list);
+	REQUIRE((void *)6 == DlistGetData(DlistPrev(DlistEnd(list))));
+	DlistPopBack(list);
+	REQUIRE((void *)10 == DlistGetData(DlistPrev(DlistEnd(list))));
+	DlistPopBack(list);
+	REQUIRE((void *)9 == DlistGetData(DlistPrev(DlistEnd(list))));
+	DlistDestroy(list);
+	return PASSED;
+}
+static test_status_t DlistSizeTest()
+{
+	Dlist_t *list = DlistCreate();
+	DlistPushBack(list, (void *)8);
+	REQUIRE(1 == DlistSize(list));
+	DlistPushBack(list, (void *)9);
+	REQUIRE(2 == DlistSize(list));
+	DlistPushBack(list, (void *)10);
+	REQUIRE(3 == DlistSize(list));
+	DlistPushBack(list, (void *)6);
+	REQUIRE(4 == DlistSize(list));
+	DlistPushBack(list, (void *)'c');
+	REQUIRE(5 == DlistSize(list));
+	DlistPushBack(list, (void *)'h');
+	REQUIRE(6 == DlistSize(list));
+	
+	DlistPopBack(list);
+	REQUIRE(5 == DlistSize(list));
+	DlistPopBack(list);
+	REQUIRE(4 == DlistSize(list));
+	DlistPopBack(list);
+	REQUIRE(3 == DlistSize(list));
+	DlistPopBack(list);
+	REQUIRE(2 == DlistSize(list));
+	DlistPopBack(list);
+	REQUIRE(1 == DlistSize(list));
+	DlistPopBack(list);
+	REQUIRE(0 == DlistSize(list));
+	DlistDestroy(list);
+	return PASSED;
+}
+static test_status_t DlistForEachTest()
+{
+	int a = 1;
+	int b = 2;
+	int c = 3;
+	int d = 4;
+	int to_add = 10;
+	Dlist_t *list = DlistCreate();
+	DlistPushFront(list,(void *)&a);
+	DlistPushFront(list,(void *)&b);
+	DlistPushFront(list,(void *)&c);
+	DlistPushFront(list,(void *)&d);
+	DlistForEach(DlistBegin(list), DlistEnd(list), UpDataByPram, (void *)&to_add);
+	REQUIRE(14 == *(int *)DlistGetData(DlistBegin(list)));
+	DlistPopFront(list);
+	REQUIRE(13 == *(int *)DlistGetData(DlistBegin(list)));
+	DlistPopFront(list);
+	REQUIRE(12 == *(int *)DlistGetData(DlistBegin(list)));
+	DlistPopFront(list);
+	REQUIRE(11 == *(int *)DlistGetData(DlistBegin(list)));
+	DlistDestroy(list);
+	return PASSED;
+}
+static test_status_t DlistFindTest()
+{
+	int a = 1;
+	int b = 2;
+	int c = 3;
+	int d = 4;
+	Dlist_t *list = DlistCreate();
+	DlistPushFront(list,(void *)&a);
+	DlistPushFront(list,(void *)&b);
+	DlistPushFront(list,(void *)&c);
+	DlistPushFront(list,(void *)&d);
+	REQUIRE(4 == *(int *)DlistGetData(DlistFind(DlistBegin(list), DlistEnd(list),MatchInt, &d )));
+	REQUIRE(3 == *(int *)DlistGetData(DlistFind(DlistBegin(list), DlistEnd(list),MatchInt, &c )));
+	REQUIRE(2 == *(int *)DlistGetData(DlistFind(DlistBegin(list), DlistEnd(list),MatchInt, &b )));
+	DlistDestroy(list);
+	return PASSED;
+}
+
+static test_status_t DlistMultiFindTest()
+{
+	int a = 1;
+	int b = 2;
+	int d = 4;
+	Dlist_t *dest = DlistCreate();
+	Dlist_t *list = DlistCreate();
+	DlistPushFront(list,(void *)&a);
+	DlistPushFront(list,(void *)&b);
+	DlistPushFront(list,(void *)&a);
+	DlistPushFront(list,(void *)&d);
+	DlistMultiFind(dest, DlistBegin(list), DlistEnd(list), MatchInt, &a);
+	REQUIRE(2 == DlistSize(dest));
+	DlistPushFront(list,(void *)&a);
+	DlistPushFront(list,(void *)&a);
+	DlistMultiFind(dest, DlistBegin(list), DlistEnd(list), MatchInt, &a);
+	REQUIRE(6 == DlistSize(dest));
+	DlistDestroy(list);
+	DlistDestroy(dest);
+	return PASSED;
+}
+
+
+
+static test_status_t DlistSpliceTest()
+{
+
+	int a = 1;
+	int b = 2;
+	int d = 4;
+	Dlist_iter_t where = NULL;
+	Dlist_iter_t from = NULL;
+	Dlist_iter_t to = NULL;
+
+
+	Dlist_t *dest = DlistCreate();
+	Dlist_t *src = DlistCreate();
+
+	to = DlistPushFront(src,(void *)&a);
+	DlistPushFront(src,(void *)&b);
+	from =DlistPushFront(src,(void *)&a);
+	DlistPushFront(src,(void *)&d);
+
+	DlistPushFront(dest,(void *)&d);
+	where = DlistPushFront(dest,(void *)&d);
+	DlistPushFront(dest,(void *)&d);
+	DlistPushFront(dest,(void *)&d);
+
+	DlistSplice(where, from, to);
+	REQUIRE(2 == DlistSize(src));
+	REQUIRE(6 == DlistSize(dest));
+	DlistDestroy(src);
+	DlistDestroy(dest);
+	return PASSED;
+}
+
+
