af5467a828381692fb6ec1f8fbedb35cd802cfeb
rotem.kadosh <rotemkadosh27@gmail.com>
2020-09-16 13:37:10 +0300

    uid approved by daher

---
+++ ds/uid/uid.c
@@ -0,0 +1,78 @@
+#include <unistd.h> /*getpid*/
+#include <limits.h> /*SHRT_MAX*/
+#include "uid.h"
+
+#define TIME_FAIL ((time_t)-1)
+#define MAX_PID SHRT_MAX
+ /*reviewed by daher*/
+
+static pid_t UIDGetPid(UID_t uid);
+static void UIDSetPid(UID_t *uid, pid_t pid);
+
+static time_t UIDGetTime(UID_t uid);
+static void UIDSetTime(UID_t *uid, time_t timestamp);
+
+static size_t UIDGetCounter(UID_t uid);
+static void UIDSetCounter(UID_t *uid, size_t counter);
+
+static int UIDIsEqualTimes(UID_t uid_one, UID_t uid_two);
+
+UID_t UIDCreate(void)
+{
+	static size_t counter = 0;
+	UID_t uid = {0};
+	time_t stamp = time(NULL);
+	if(TIME_FAIL ==  stamp)
+	{
+		return UIDGetBadUid();
+	}
+	UIDSetTime(&uid, stamp);
+	UIDSetPid(&uid,getpid());
+	UIDSetCounter(&uid, ++counter);
+	return uid;
+}
+static pid_t UIDGetPid(UID_t uid)
+{
+	return uid.pid;
+}
+static void UIDSetPid(UID_t *uid, pid_t pid)
+{
+	uid->pid = pid;
+}
+static time_t UIDGetTime(UID_t uid)
+{
+	return uid.time_stamp;
+}
+static void UIDSetTime(UID_t *uid, time_t timestamp)
+{
+	uid->time_stamp = timestamp;
+}
+static size_t UIDGetCounter(UID_t uid)
+{
+	return uid.counter;
+}
+static void UIDSetCounter(UID_t *uid, size_t counter)
+{
+	uid->counter = counter;
+}
+static int UIDIsEqualTimes(UID_t uid_one, UID_t uid_two)
+{
+	return (UIDGetTime(uid_one) == UIDGetTime(uid_two));
+}
+int UIDIsSame(UID_t one, UID_t two)
+{
+
+	return ((UIDGetPid(one) == UIDGetPid(two)) && UIDIsEqualTimes(one, two) && (UIDGetCounter(one) == UIDGetCounter(two)));
+}
+UID_t UIDGetBadUid(void)
+{
+	UID_t uid = {0};
+	return uid;
+}
+
+/*
+static int UIDIsBadUid(UID_t uid)
+{
+	return (0 == UIDGetCounter(uid) && 0 == UIDGetTime(uid) && 0 == UIDGetPid(uid));
+}
+*/
---
+++ ds/uid/uid_test.c
@@ -0,0 +1,40 @@
+
+#include "../utils/test.h" /*REQUIRE RUNTEST*/
+#include <unistd.h> /*getpid*/
+#include <stdio.h>
+#include "uid.h"
+
+#define TRUE (1)
+#define FALSE (0)
+
+int UIDtest(void);
+
+int main()
+{
+	RUNTEST(UIDtest);
+	return 0;
+}
+
+int UIDtest(void)
+{
+	UID_t bad_uid = UIDGetBadUid();
+	UID_t uid = UIDCreate();
+	UID_t uid2 = UIDCreate();
+	UID_t uid3 = UIDCreate();
+
+	REQUIRE(time(NULL) == uid.time_stamp);
+	REQUIRE(1 == uid.counter);
+	REQUIRE(getpid() == uid.pid);
+	
+	REQUIRE(2 == uid2.counter);
+	REQUIRE(3 == uid3.counter);
+
+	REQUIRE(0 == bad_uid.pid);
+	REQUIRE(0 == bad_uid.time_stamp);
+	REQUIRE(0 == bad_uid.counter);
+
+	REQUIRE(TRUE == UIDIsSame(uid, uid));
+	REQUIRE(FALSE == UIDIsSame(uid, bad_uid));
+
+	return PASSED;
+}
