b509a80cfdaf3c3f7138673e06ddd805e7346833
rotem.kadosh <rotemkadosh27@gmail.com>
2020-11-04 08:45:58 +0200

    befor butify dhcp, 4/11 - 8:45

--- ds/DHCP/dhcp_test.c
+++ ds/DHCP/dhcp_test.c
@@ -18,77 +18,108 @@ int main()
     return 0;
 }
 
-static test_status_t AllocateTest(void)
+static test_status_t AllocateFreeTest(void)
 {
     int status = 0;
     char buffer[13] = "196.0.63.0";
+    char buffer_req[13] = "196.0.63.4";
+
     char buffer1[13] = "196.0.63.1";
     char buffer2[13] = "196.0.63.2";
     char buffer3[13] = "196.0.63.3";
-    char buffer4[13] = "196.0.63.6";
     char buffer5[13] = "196.0.63.5";
-    char buffer6[13] = "196.0.63.7";
-    char buffer_req[13] = "196.0.63.4";
+    char buffer6[13] = "196.0.63.6";
+
     ip_ty subnet = StringToIp(buffer);
     ip_ty requested = StringToIp(buffer_req);
+
     ip_ty ans = 0;
     ip_ty ans2 = 0;
-    int number_of_bit_net = 28;
+
+    int number_of_bit_net = 29;
+
     Dhcp_ty *dhpc = DhcpCreate(subnet, number_of_bit_net);
-       
+    REQUIRE(DhcpCountFree(dhpc) == 6);
+
     /*"196.0.63.4"*/
     status = DhcpAllocateIp(dhpc, &ans, requested);
     REQUIRE(requested == ans);
     REQUIRE(status == SUCCESS);
+    REQUIRE(DhcpCountFree(dhpc) == 5);
 
     /*"196.0.63.1"*/
     ans = 0;
+    ans2 = StringToIp(buffer1);
     status = DhcpAllocateIp(dhpc, &ans, 0);
+    REQUIRE(ans == ans2);
     REQUIRE(status == SUCCESS);
+    REQUIRE(DhcpCountFree(dhpc) == 4);
+
     status = DhcpRFreeIp(dhpc, ans);
     REQUIRE(status == SUCCESS);
+    REQUIRE(DhcpCountFree(dhpc) == 5);
+
+
     status = DhcpRFreeIp(dhpc, ans);
     REQUIRE(status == DOUBLE_FREE);
+    REQUIRE(DhcpCountFree(dhpc) == 5);
+
+
     ans2 = 3254525365;
     status = DhcpRFreeIp(dhpc, ans2);
     REQUIRE(status == DOUBLE_FREE);
-    
+    REQUIRE(DhcpCountFree(dhpc) == 5);
     
     /*"196.0.63.1"*/
     ans2 = 0;
     status = DhcpAllocateIp(dhpc, &ans2, 0);
     REQUIRE(status == SUCCESS);
     REQUIRE(ans == ans2);
-    
+    REQUIRE(DhcpCountFree(dhpc) == 4);
     
     /*"196.0.63.2"*/
     ans = 0;
+    ans2 = StringToIp(buffer2);
     status = DhcpAllocateIp(dhpc, &ans, 0);
+    REQUIRE(ans == ans2);
     REQUIRE(status == SUCCESS);
-
+    REQUIRE(DhcpCountFree(dhpc) == 3);
 
     /*"196.0.63.3"*/
     ans = 0;
+    ans2 = StringToIp(buffer3);
     status = DhcpAllocateIp(dhpc, &ans, requested);
+    REQUIRE(ans == ans2);
     REQUIRE(status == SUCCESS);
+    REQUIRE(DhcpCountFree(dhpc) == 2);
 
     /*"196.0.63.5"*/
     ans = 0;
+    ans2 = StringToIp(buffer5);
     status = DhcpAllocateIp(dhpc, &ans, 0);
+    REQUIRE(ans == ans2);
     REQUIRE(status == SUCCESS);
+    REQUIRE(DhcpCountFree(dhpc) == 1);
 
     /*"196.0.63.6"*/
-     ans = 0;
+    ans = 0;
+    ans2 = StringToIp(buffer6);
     status = DhcpAllocateIp(dhpc, &ans, 0);
     REQUIRE(status == SUCCESS);
+    REQUIRE(ans == ans2);
+    REQUIRE(DhcpCountFree(dhpc) == 0);
+
 
     /*"196.0.63.7"*/
     ans = 0;
     status = DhcpAllocateIp(dhpc, &ans, 0);
-    REQUIRE(status == SUCCESS);
+    REQUIRE(status == MALLOC_FAILURE);
+    REQUIRE( 0 == DhcpCountFree(dhpc));
 
-    printf("%ld\n",DhcpCountFree(dhpc));
-    REQUIRE( 7 == DhcpCountFree(dhpc));
+    ans = ans2;
+    status = DhcpRFreeIp(dhpc, ans);
+    REQUIRE(status == SUCCESS);
+    REQUIRE(DhcpCountFree(dhpc) == 1);
 
     
     return PASSED;
